25d8554fc866729a75487ef93e53b53186173ec8
Dforce <kcy493@gmail.com>
2025-06-06 03:06:27 +0500

    Update MainActivity.kt
    Fix Major Bugs

--- android/app/src/main/kotlin/com/nb/flutteruishopping/MainActivity.kt
+++ android/app/src/main/kotlin/com/nb/flutteruishopping/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nb.flutteruishopping
 import android.os.Bundle
 
 import io.flutter.embedding.android.FlutterActivity
+"tVvJ9c" = "Desktop-Widget-Konfiguration";
 
 
 class MainActivity: FlutterActivity() {
